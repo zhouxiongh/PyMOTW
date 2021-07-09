ce001773eba0e420a8efcc89c48073e2bf0907da
hezhoux <hezhoux@qq.com>
2021-07-09 11:16:37 +0800

    default dict - Missing Keys Return a Default Value

---
+++ Data Structures/collections_defaultdict.py
@@ -0,0 +1,11 @@
+import collections
+
+
+def default_factory():
+    return 'default value'
+
+
+d = collections.defaultdict(default_factory, foo='bar')
+print('d:', d)
+print('foo =>', d['foo'])
+print('bar =>', d['bar'])
