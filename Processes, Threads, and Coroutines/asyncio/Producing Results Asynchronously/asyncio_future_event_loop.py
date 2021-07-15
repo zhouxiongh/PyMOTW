f65d951c5cbf6e920ff239650fbe7c9b3b63e9dd
hezhoux <hezhoux@qq.com>
2021-07-15 11:46:32 +0800

    Producing Results Asynchronously

---
+++ Processes, Threads, and Coroutines/asyncio/Producing Results Asynchronously/asyncio_future_event_loop.py
@@ -0,0 +1,28 @@
+import asyncio
+
+"""A Future represents the result of work that has not been completed yet. 
+The event loop can watch for a Future object’s state to indicate that it is 
+done, allowing one part of an application to wait for another part to finish 
+some work """
+
+
+def mark_done(future, result):
+    print('setting future result to {!r}'.format(result))
+    future.set_result(result)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    all_done = asyncio.Future()
+
+    print('scheduling mark_done')
+    event_loop.call_soon(mark_done, all_done, 'the result')
+
+    print('entering event loop')
+    result = event_loop.run_until_complete(all_done)
+    print('returned result: {!r}'.format(result))
+finally:
+    print('closing event loop')
+    event_loop.close()
+
+print('future result: {!r}'.format(all_done.result()))
