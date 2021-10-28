5f6811258f2ad050b68f261f3e74f441ea17cfd1
Jason He <hezhoux@qq.com>
2021-10-29 00:55:03 +0800

    use default dict declare two dimensional dict

--- Data Structures/collections/collections_defaultdict.py
+++ Data Structures/collections/collections_defaultdict.py
@@ -9,3 +9,9 @@ d = collections.defaultdict(default_factory, foo='bar')
 print('d:', d)
 print('foo =>', d['foo'])
 print('bar =>', d['bar'])
+
+# two dimensional dict
+d = collections.defaultdict(dict)
+d['a']['b'] = 1
+print("a in d", 'a' in d)
+print("b in d", 'b' in d)
