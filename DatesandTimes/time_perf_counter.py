9f3ae8531e8af671301f9af5dafe39ca9b2dbda8
hezhoux <hezhoux@qq.com>
2022-01-30 13:29:56 +0800

    rename dir

--- DatesandTimes/time_perf_counter.py
+++ DatesandTimes/time_perf_counter.py
@@ -1,7 +1,7 @@
 import hashlib
 import time
 
-data = open(__file__, 'rb').read()
+data = open(__file__, "rb").read()
 
 loop_start = time.perf_counter()
 
@@ -14,4 +14,4 @@ for i in range(5):
     now = time.perf_counter()
     loop_elpased = now - loop_start
     iter_elpased = now - iter_start
-    print(time.ctime(), ': {:0.3f} {:0.3f}'.format(iter_elpased, loop_elpased))
+    print(time.ctime(), ": {:0.3f} {:0.3f}".format(iter_elpased, loop_elpased))
---
+++ DatesandTimes/time_timezone.py
@@ -0,0 +1,27 @@
+# time_timezone.py
+import time
+import os
+
+
+def show_zone_info():
+    print("  TZ    :", os.environ.get("TZ", "(not set)"))
+    print("  tzname:", time.tzname)
+    print("  Zone  : {} ({})".format(time.timezone, (time.timezone / 3600)))
+    print("  DST   :", time.daylight)
+    print("  Time  :", time.ctime())
+    print()
+
+
+print("Default :")
+show_zone_info()
+
+ZONES = [
+    "GMT",
+    "Europe/Amsterdam",
+]
+
+for zone in ZONES:
+    os.environ["TZ"] = zone
+    time.tzset()
+    print(zone, ":")
+    show_zone_info()
